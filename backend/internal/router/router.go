ececb0874506397b027184184d073444217feb34
chitresh99 <chitreshpoojary990@gmail.com>
2025-08-14 01:04:12 +0530

    fix: add cors handling

--- backend/internal/router/router.go
+++ backend/internal/router/router.go
@@ -2,12 +2,31 @@ package router
 
 import (
 	"backend/internal/handler"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
+func CORSMiddleware() gin.HandlerFunc {
+	return gin.HandlerFunc(func(c *gin.Context) {
+		c.Header("Access-Control-Allow-Origin", "http://localhost:3000")
+		c.Header("Access-Control-Allow-Credentials", "true")
+		c.Header("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Header("Access-Control-Allow-Methods", "POST, HEAD, PATCH, OPTIONS, GET, PUT, DELETE")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(http.StatusNoContent)
+			return
+		}
+
+		c.Next()
+	})
+}
+
 func SetupRouter() *gin.Engine {
 	r := gin.Default()
+
+	r.Use(CORSMiddleware())
 
 	linearHandler := handler.NewLinearHandler()
 	statsHandler := handler.NewStatsHandler()
